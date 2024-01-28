8e0130ed31c12f531d39f8893dc731fde1c3481c
joe-loach <joe.gloach@gmail.com>
2024-01-28 20:48:34 +0000

    actually sample from colour_texture

--- crates/marcher/src/lib.rs
+++ crates/marcher/src/lib.rs
@@ -19,22 +19,7 @@ impl Marcher {
     pub fn new(device: &wgpu::Device) -> Self {
         let pipeline = shader::compute::create_comp_pipeline(device);
 
-        let tex = device.create_texture(&TextureDescriptor {
-            label: None,
-            size: wgpu::Extent3d {
-                width: 1,
-                height: 1,
-                depth_or_array_layers: 1,
-            },
-            mip_level_count: 1,
-            sample_count: 1,
-            dimension: wgpu::TextureDimension::D2,
-            format: wgpu::TextureFormat::Rgba16Float,
-            usage: wgpu::TextureUsages::STORAGE_BINDING
-                | wgpu::TextureUsages::COPY_SRC
-                | wgpu::TextureUsages::TEXTURE_BINDING,
-            view_formats: &[],
-        });
+        let tex = device.create_texture(&texture_descriptor());
 
         Self { pipeline, tex }
     }
@@ -43,18 +28,12 @@ impl Marcher {
         if width != self.tex.width() || height != self.tex.height() {
             // recreate texture
             self.tex = device.create_texture(&TextureDescriptor {
-                label: None,
                 size: wgpu::Extent3d {
                     width,
                     height,
                     depth_or_array_layers: 1,
                 },
-                mip_level_count: 1,
-                sample_count: 1,
-                dimension: wgpu::TextureDimension::D2,
-                format: wgpu::TextureFormat::Rgba16Float,
-                usage: wgpu::TextureUsages::STORAGE_BINDING | wgpu::TextureUsages::COPY_SRC,
-                view_formats: &[],
+                ..texture_descriptor()
             });
         }
     }
@@ -84,5 +63,24 @@ impl Marcher {
 
         let [x, y, _z] = shader::compute::COMP_WORKGROUP_SIZE;
         pass.dispatch_workgroups(width / x, height / y, 1);
+    }
+}
+
+fn texture_descriptor() -> wgpu::TextureDescriptor<'static> {
+    wgpu::TextureDescriptor {
+        label: None,
+        size: wgpu::Extent3d {
+            width: 1,
+            height: 1,
+            depth_or_array_layers: 1,
+        },
+        mip_level_count: 1,
+        sample_count: 1,
+        dimension: wgpu::TextureDimension::D2,
+        format: wgpu::TextureFormat::Rgba16Float,
+        usage: wgpu::TextureUsages::STORAGE_BINDING
+            | wgpu::TextureUsages::COPY_SRC
+            | wgpu::TextureUsages::TEXTURE_BINDING,
+        view_formats: &[],
     }
 }
--- sim/src/fullscreen/shader.wgsl
+++ sim/src/fullscreen/shader.wgsl
@@ -21,8 +21,7 @@ var color_sampler: sampler;
 
 @fragment
 fn frag(in: VertexOutput) -> @location(0) vec4<f32> {
-    // let color = textureSample(color_texture, color_sampler, in.uv).rgb;
-    // return vec4<f32>(color, 1.0);
-    return vec4<f32>(1.0, 0.0, 0.0, 1.0);
+    let color = textureSample(color_texture, color_sampler, in.uv).rgb;
+    return vec4<f32>(color, 1.0);
 }
 
