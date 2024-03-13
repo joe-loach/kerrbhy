b2953f524468d5af9842e6120682665c4fb4f670
joe-loach <joe.gloach@gmail.com>
2024-03-13 16:00:48 +0000

    use sliders instead and remove separators

--- sim/src/ui/config.rs
+++ sim/src/ui/config.rs
@@ -4,8 +4,6 @@ use common::{
 };
 
 pub fn show(ui: &mut egui::Ui, cfg: &mut Config) {
-    ui.separator();
-
     ui.vertical(|ui| {
         ui.label("Features:");
         for (name, f) in Features::all().iter_names() {
@@ -23,19 +21,7 @@ pub fn show(ui: &mut egui::Ui, cfg: &mut Config) {
         ui.set_enabled(cfg.features.contains(Features::DISK));
 
         ui.label("Disk");
-        ui.add(
-            egui::DragValue::new(&mut cfg.disk.radius)
-                .speed(0.1)
-                .prefix("Radius: ")
-                .clamp_range(0.0..=10.0),
-        );
-        ui.add(
-            egui::DragValue::new(&mut cfg.disk.thickness)
-                .speed(0.1)
-                .prefix("Thickness: ")
-                .clamp_range(0.0..=10.0),
-        );
+        ui.add(egui::Slider::new(&mut cfg.disk.radius, 0.0..=10.0).text("Radius"));
+        ui.add(egui::Slider::new(&mut cfg.disk.thickness, 0.0..=10.0).text("Thickness"));
     });
-
-    ui.separator();
 }
